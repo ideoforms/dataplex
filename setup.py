b990632c5297919929f14f21ced734784d030567
Daniel Jones <daniel@jones.org.uk>
2024-06-27 22:54:44 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name = 'dataplex',
+    version = '0.0.1',
+    description = 'dataplex, a data I/O hub designed for real-time analysis and performance systems',
+    long_description = open("README.md", "r").read(),
+    long_description_content_type = "text/markdown",
+    author = 'Daniel Jones',
+    author_email = 'dan-code@erase.net',
+    url = 'https://github.com/ideoforms/dataplex',
+    packages = ['dataplex'],
+    keywords = ('data', 'analysis', 'statistics', 'sound', 'music'),
+    install_requires = ["pydantic", "pyserial", "python-osc", "numpy", "jdp"],
+    classifiers = [
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Artistic Software',
+        'Topic :: Communications',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    ]
+)
