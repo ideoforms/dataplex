47bd5f6f412ebae71999baccbcc66c0350d26a44
Daniel Jones <daniel@jones.org.uk>
2024-07-02 23:04:17 +0000

    Rethink Client as a data source

--- dataplex/client.py
+++ dataplex/client.py
@@ -1,47 +1,10 @@
-from simutils import AttributeDictionary
-
 import jdp
-import time
 
-class Client (object):
-    def __init__(self):
-        self.server = jdp.Server(11000)
-        self.server.add_callback(self.handler)
-        self.server.start()
-        self.data = AttributeDictionary()
-        self.callbacks = []
+class Client:
+    def __init__(self,
+                 server_host: str = "127.0.0.1",
+                 server_port: int = 11000):
+        self.server = jdp.Client((server_host, server_port))
 
-    def __iter__(self):
-        return iter(self.data)
-    
-    def wait_for_data(self, num_fields = 1):
-        while not len(list(self.data.keys())) >= num_fields:
-            print("Waiting for data..")
-            time.sleep(0.1)
-        print("Got data: %s" % list(self.data.keys()))
-
-    def handler(self, data):
-        for key, value in list(data.items()):
-            self.data.set(key, value)
-        for callback in self.callbacks:
-            callback(data)
-    
-    def add_callback(self, callback):
-        self.callbacks.append(callback)
-
-    def remove_callback(self, callback):
-        self.callbacks.remove(callback)
-    
-    def get_data(self):
-        return self.data
-
-    def get_value(self, condition):
-        d = self.data.get(condition)
-        if d:
-            return d["value"]
-
-    def get_normalised(self, condition):
-        d = self.data.get(condition)
-        if d:
-            return d["normalised"]
-
+    def send(self, data: dict):
+        self.server.send(data)
